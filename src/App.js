be7414d99da7d83ac877a4c149b679893474b3ed
Tom Hoad <40165392+tomhoad03@users.noreply.github.com>
2023-04-27 13:56:30 +0100

    Test 2

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>
+          Test 2
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
